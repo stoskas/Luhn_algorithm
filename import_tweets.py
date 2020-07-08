aae457ccaf86543156f935130fcf43b47b01b0a8
stoskas <44064543+stoskas@users.noreply.github.com>
2020-07-08 11:11:01 +0300

    Add files via upload

--- import_tweets.py
+++ import_tweets.py
@@ -1,13 +1,13 @@
 import tweepy 
   
 # Exo valei ta dika mou credentials apo to api tou tweeter
-consumer_key = "" 
-consumer_secret = ""
-access_key = ""
-access_secret = ""
+consumer_key = "oUNpdHhYxlZ4zgP512hQmbeQv" 
+consumer_secret = "D4hpg7NknxXHKqkGrUk9EWhP2CvaXugk6hHH5fIs1KtT3ZbMkF"
+access_key = "2244306962-ORcN9ROKsdRAEyjOsduCjzvMtTf24HT7IGSWtWE"
+access_secret = "vvs2MIDTXEQ1k9UWAGBNFg1eh4qw59hk1Ij7WUX7lB9eD"
   
 # Function to extract tweets 
-def get_tweets(username): 
+def get_tweets(userID): 
           
         # Authorization to consumer key and consumer secret 
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
@@ -17,27 +17,26 @@ def get_tweets(username):
   
         # Calling api 
         api = tweepy.API(auth) 
-  
-        # 200 tweets to be extracted 
-        number_of_tweets=50
-        tweets = api.user_timeline(screen_name= username) 
-  
+        
+        tweets = api.user_timeline(screen_name= userID, count=50, include_rts = False, tweet_mode = 'extended')
         # Empty Array 
         tmp=[]  
+        
   
         # create array of tweet information: username,  
         # tweet id, date/time, text 
-        tweets_for_csv = [tweet.text for tweet in tweets] # CSV file created  
-        for j in tweets_for_csv: 
+        # tweets_for_csv = [tweet.text for tweet in tweets] # CSV file created  
+        for j in tweets: 
   
             # Appending tweets to the empty array tmp 
-            tmp.append(j)
+            tmp.append(j.full_text)
             
   
         # Printing the tweets 
-        print(tmp) 
-        
-
+        print(tmp)
+        x=len(tmp)
+        print(x)
+    
   
 # Driver code 
 if __name__ == '__main__': 
